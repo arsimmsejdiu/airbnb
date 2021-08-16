31ab6a605a2bffa3d8307ace4b691edf039ed336
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-08-16 22:07:29 +0200

    added react tilt for images

--- components/LargeCard.js
+++ components/LargeCard.js
@@ -2,9 +2,8 @@ import React from "react";
 import Image from "next/image";
 
 function LargeCard({ img, title, description, buttonText }) {
-  console.log(img);
   return (
-    <section className="relative py-16 cursor-pointer">
+    <div className="relative py-16 cursor-pointer">
       <div className="relative h-96 min-w-[300px]">
         <Image src={img} layout="fill" objectFit="cover" className="rounded-2xl"/>
       </div>
@@ -13,7 +12,7 @@ function LargeCard({ img, title, description, buttonText }) {
         <p>{description}</p>
         <button className="text-sm text-white bg-gray-900 px-4 py-2 rounded-lg mt-5">{buttonText}</button>
       </div>
-    </section>
+    </div>
   );
 }
 
--- components/SmallCard.js
+++ components/SmallCard.js
@@ -1,13 +1,14 @@
 import Image from 'next/image';
+import Tilt from 'react-parallax-tilt';
 
 function Smallcard({ img, location, distance }) {
   return (
     <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer 
       hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out">
       {/* left */}
-      <div className="relative h-16 w-16">
+      <Tilt options={{ max : 25 }} className="relative h-16 w-16">
           <Image src={img} layout="fill" className="rounded-lg" />
-      </div>
+      </Tilt>
       {/* right */}
       <div>
           <h2>{location}</h2>
