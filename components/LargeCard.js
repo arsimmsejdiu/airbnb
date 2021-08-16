a88086794710eb142f96c36d21c2feafea589975
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-08-16 16:45:58 +0200

    Added the live anywhere section

---
+++ components/LargeCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LargeCard() {
+  return (
+    <div>
+      <h1>Large Card Component</h1>
+    </div>
+  );
+}
+
+export default LargeCard;
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import Banner from "../components/Banner";
 import Smallcard from "../components/SmallCard";
 import MediumCard from "../components/MediumCard";
+import LargeCard from "../components/LargeCard";
 
 export default function Home({ exploreData, cardsData }) {
   return (
@@ -35,9 +36,15 @@ export default function Home({ exploreData, cardsData }) {
 
         <section>
           <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
-          {cardsData?.map(({ img, title }) => (
-            <MediumCard key={img} img={img} title={title} />
-          ))}
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {cardsData?.map(({ img, title }) => (
+              <MediumCard key={img} img={img} title={title} />
+            ))}
+          </div>
+        </section>
+
+        <section>
+          <LargeCard />
         </section>
       </main>
     </div>
