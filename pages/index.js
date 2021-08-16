887d161e2e962172f2d60d89eb6f30078314995a
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-08-16 13:38:40 +0200

    main section

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,16 @@ export default function Home() {
 
       {/* Header */}
       <Header />
+
       {/* Banner */}
       <Banner />
+
+      {/* Main Section */}
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+        </section>
+      </main>
     </div>
   );
 }
