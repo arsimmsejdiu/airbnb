ed1f87f8a45dda378fe5530c805f48e05fef34c2
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-09-06 16:44:07 +0200

    little changes

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home({ exploreData, cardsData }) {
   return (
     <div className="">
       <Head>
-        <title>Airbnb</title>
+        <title>Airbnb - Explore and Adventure</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
