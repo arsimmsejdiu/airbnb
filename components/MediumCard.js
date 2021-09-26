a33cca6e35891f380450aeb9c60dcfc5cfaab0d5
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-09-26 15:31:40 +0200

    Changed the transitions in the medium cards

--- components/MediumCard.js
+++ components/MediumCard.js
@@ -6,10 +6,10 @@ function MediumCard({ img, title }) {
     //global styles at styles/global.css
     <main className="flex items-center justify-center	h-full w-full">
       <div options={{ max: 10 }} className="relative h-80 w-80 hover:scale-105 transition transform duration-200 ease-out">
-        <div className="overflow-hidden relative justify-end cursor-pointer capitalize flex flex-col rounded-md w-60 h-80 z-50 object-cover text-white">
+        <div className="medium-card ">
           <Image src={img} alt={title} layout="fill" />
           <div className="absolute p-3 flex flex-col">
-            <span className="text-xs bg-gray-50 text-gray-700 w-28 p-1 opacity-75 hover:opacity-80 rounded-md text-center">{title}</span>
+            <span className="text-xs bg-gray-50 text-gray-800 w-28 p-1 opacity-75 hover:opacity-80 rounded-md text-center">{title}</span>
             <h1 className="text-xl font-bold">Independent luxury studio apartment</h1>
             <span className="flex justify-between">
               <p className="text-xs ">Anderson Pareira</p>
